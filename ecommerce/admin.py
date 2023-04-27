7b85b83fe95b37aa7bd5799a31505a2ebc0d9eb8
Talha Shahid <talhamalik25.tm@gmail.com>
2023-04-27 17:01:02 +0500

    Fixed admin error _

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
 
-from .models import Type, Category, Product, OrderItem, Order, Images
+
+from .models import Type, Category, Product, OrderItem, Order, Images, Material
 
 # Register your models here.
 
@@ -10,6 +12,14 @@ admin.site.register(Category)
 admin.site.register(Type)
 admin.site.register(OrderItem)
 admin.site.register(Order)
+admin.site.register(Material)
+
+
+# Image Model
+class ImageInline (admin.TabularInline):
+	model = Images
+	fk_name = 'img'
+	verbose_name_plural = _('image')
 
 class ImageAdmin (admin.ModelAdmin):
 	list_display = ('item', 'image_tag')
