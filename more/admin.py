0f73be48fa747baa697b9c76044475042a652e38
Talha Malik <talhamalik25.tm@gmail.com>
2023-07-11 15:43:40 +0500

    Added more models to admin.py

--- more/admin.py
+++ more/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import NewsLetter
+from .models import NewsLetter, BecomeSeller, BecomeSellerImage
 # Register your models here.
 admin.site.register(NewsLetter)
+admin.site.register(BecomeSeller)
+admin.site.register(BecomeSellerImage)
