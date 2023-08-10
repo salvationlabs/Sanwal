fa2445f5a49531209fdced3fa71feb3612ce48c0
Talha Malik <talhamalik25.tm@gmail.com>
2023-08-11 04:38:29 +0500

    Changed font, size of heading

--- core/settings/base.py
+++ core/settings/base.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Application definition
